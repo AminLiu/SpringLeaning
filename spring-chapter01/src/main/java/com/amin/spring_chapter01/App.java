9ba98937429fc699efa9191183155711923f6373
amin <122328576@qq.com>
2017-06-08 11:51:00 +0800

    首次提交

---
+++ spring-chapter01/src/main/java/com/amin/spring_chapter01/App.java
@@ -0,0 +1,13 @@
+package com.amin.spring_chapter01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
